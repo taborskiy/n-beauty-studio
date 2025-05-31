177ca38c95a2c62631dc21ba5046de7a5983ca8c
Marian <taborskiy.marian@gmail.com>
2025-05-31 09:55:06 +0300

    test commit 5

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <header class="bg-white shadow-sm">
       <nav class="container mx-auto px-4 py-4">
         <div class="flex justify-between items-center">
-          <NuxtLink to="/" class="text-2xl font-light text-gray-800">N-Beauty Studio</NuxtLink>
+          <NuxtLink to="/" class="text-2xl font-light text-gray-800">N-Beauty Studio2</NuxtLink>
           <div class="space-x-6">
             <NuxtLink to="/" class="text-gray-600 hover:text-gray-900">Home</NuxtLink>
             <NuxtLink to="/gallery" class="text-gray-600 hover:text-gray-900">Gallery</NuxtLink>
